c8bd49d1a92970a81688bd2c2d65b19b887f7327
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-02-28 08:30:32 -0500

    Some housekeeping.

---
+++ sandman/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Welcome to Sandman.</h1>
+            <h3>Please Log In.</h3>
+        </div>
+    )
+}
+
+export default Login;
