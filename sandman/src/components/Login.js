5b0e696a3f4a4b669c056df58b233a313cf6b5a6
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-02-27 21:44:54 -0500

    Initial staging branch commit

---
+++ sandman/src/components/Login.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    return (
+        <>
+            <h1>Welcome to Sandman.</h1>
+            <h3>Please Log In.</h3>
+        </>
+    )
+}
