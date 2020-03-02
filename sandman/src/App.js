48ac552a46313de0c21c2c4b29b0d0438142cfc8
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-03-01 22:56:26 -0500

    Basic styling done. A navigation created, routes added. I am not an expert on designing from scratch so it looks like a toddler made it. Still it is a start. Added logout button as well but commented out until API is working to test it.

--- sandman/src/App.js
+++ sandman/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-//Erik's imports
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 
-//Erik's imports
-import Login from './components/Login'
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        {/* <Route path="/register" component={Register} /> */}
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
   );
 }
