e869cc7414b4e829b3ac6a126aff7d9ade4c6911
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-03-03 20:58:01 -0700

    Have rough template for chart.js chart.  Need to connect to back-end, using sample data now.  Need to figure out military time calculation, maybe condense/refactor code.

--- sandman/src/App.js
+++ sandman/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import SleepTracker from './components/SleepTracker';
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <Route exact path="/" component={Login} />
         {/* <Route path="/register" component={Register} /> */}
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/tracker" component={SleepTracker} />
       </Switch>
     </div>
   );
---
+++ sandman/src/components/SleepTracker.js
@@ -0,0 +1,125 @@
+import React, { createRef, Component } from 'react';
+import Chart from 'chart.js';
+// import classes from "./LineGraph.module.css";
+
+const data = [
+    {
+        date: '2/26/2020',
+        sleepStart: 0,
+        sleepEnd: 7,
+        moodMorn: 2,
+        moodMid: 2,
+        moodNight: 4
+    },
+    {
+        date: '2/27/2020',
+        sleepStart: 18,
+        sleepEnd: 23,
+        moodMorn: 3,
+        moodMid: 1,
+        moodNight: 3
+    },
+    {
+        date: '2/28/2020',
+        sleepStart: 22,
+        sleepEnd: 6,
+        moodMorn: 4,
+        moodMid: 4,
+        moodNight: 2
+    },
+    {
+        date: '2/29/2020',
+        sleepStart: 10,
+        sleepEnd: 18,
+        moodMorn: 4,
+        moodMid: 2,
+        moodNight: 3
+    },
+    {
+        date: '3/01/2020',
+        sleepStart: 14,
+        sleepEnd: 16,
+        moodMorn: 1,
+        moodMid: 1,
+        moodNight: 1
+    }
+];
+
+const mySleepData = data.map(sleepDay => {
+    const sleepStartHours = sleepDay.sleepStart;
+    const sleepEndHours = sleepDay.sleepEnd;
+
+    if (sleepStartHours === 0 && sleepEndHours < 12) {
+        return sleepEndHours;
+    }
+    else if (sleepStartHours === 0 && sleepEndHours > 12) {
+        const totalHours = (sleepEndHours - 12) + sleepStartHours;
+        return totalHours;
+    }
+    else if (sleepStartHours > 12 && sleepEndHours < 12) {
+        const totalHours = (24 - sleepStartHours) + sleepEndHours;
+        return totalHours;
+    }
+    else if (sleepStartHours > 12 && sleepEndHours > 12) {
+        const totalHours = (sleepEndHours - 12) - (sleepStartHours - 12);
+        return totalHours;
+    }
+    else if (sleepStartHours < 12 && sleepEndHours > 12) {
+        const totalHours = (12 - sleepStartHours) + (sleepEndHours - 12);
+        return totalHours;
+    }
+});
+
+const mySleepLabels = data.map(sleepDay => {
+    return sleepDay.date;
+});
+
+
+class SleepTracker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            sleepData: mySleepData,
+            sleepLabels: mySleepLabels
+        };
+    }
+
+    chartRef = createRef();
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: "bar",
+            data: {
+                //Bring in data
+                labels: this.state.sleepLabels,
+                datasets: [
+                    {
+                        label: "Hours of Sleep",
+                        data: this.state.sleepData,
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Sleep Tracker!</h1>
+                <div>
+                    <canvas
+                        id="myChart"
+                        ref={this.chartRef}
+                    />
+                </div>
+            </>
+        );
+    }
+};
+
+export default SleepTracker;
