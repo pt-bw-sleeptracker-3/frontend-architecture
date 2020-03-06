a3ab48b208ea44ae0687138b380503f456fdafce
Erik Scow <erikscow@gmail.com>
2020-03-05 20:07:44 -0800

    Moved api call to dashboard.

--- sandman/src/utils/PrivateRoute.js
+++ sandman/src/utils/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ componenet: Component, ...theRest }) => {
+const PrivateRoute = ({ component: Component, ...theRest }) => {
     return (
         <Route
             {...theRest}
